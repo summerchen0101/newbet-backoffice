e56366737ef814cc904e1a157e0ebe4477c5ae20
Summer Chen <mp4bj6ruby@gmail.com>
2020-09-21 22:11:12 +0800

    basic dashboard style

---
+++ src/app/containers/APP/sample.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { User } from 'app/components/User';
+// import { IInitialState } from './store/reducer/count'
+import { addCounter, subCounter, userFetchRequest } from 'store/actions';
+import { RootState } from 'store/index';
+
+const App: React.FC<IProps> = (props) => (
+  <div>
+    <h2>{props.counter}</h2>
+    <button onClick={() => props.onAdd(10)}>ADD</button>
+    <button onClick={() => props.onSub(10)}>SUB</button>
+    <button onClick={() => props.onFetchUser(1)}>GET USER 1</button>
+    <button onClick={() => props.onFetchUser(2)}>GET USER 2</button>
+    <div>{props.framework}</div>
+    <div>{props.user?.name}</div>
+    <div>{props.user?.email}</div>
+    <User name="summer" age={20} specialize="economy" />
+  </div>
+);
+
+const mapStateToProps = (state: RootState) => ({
+  counter: state.count.counter,
+  user: state.user.user,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onAdd: (num) => dispatch(addCounter(num)),
+  onSub: (num) => dispatch(subCounter(num)),
+  onFetchUser: (id) => dispatch(userFetchRequest(id)),
+});
+
+type IProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps> & {
+    compiler: string;
+    framework: string;
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
+import Dashboard from 'app/components/Dashboard/index';
 import { store } from './store';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App compiler="TypeScript" framework="React" />
+    <Dashboard>
+      <App compiler="TypeScript" framework="React" />
+    </Dashboard>
   </Provider>,
   document.getElementById('root'),
 );
