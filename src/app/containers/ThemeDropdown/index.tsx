092bac90d9c1a577c13b924623784153ba6e6cda
Summer Chen <mp4bj6ruby@gmail.com>
2020-10-14 10:55:05 +0800

    use redux hook in ThemeDropdown

--- src/app/containers/ThemeDropdown/index.tsx
+++ src/app/containers/ThemeDropdown/index.tsx
@@ -1,31 +1,15 @@
 import Wrapper from './Wrapper';
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { makeSelectTheme } from 'app/store/style/selector';
-import { changeTheme } from 'store/style/actions';
 import makeDropdownMenu from './makeDropdownMenu';
 
-const Component: React.FC<Props> = (props) => {
-  useEffect(() => {
-    console.log('[ThemeDropdown] useEffect');
-  });
-  const menu = makeDropdownMenu(props.changeTheme);
-  return <Wrapper title={props.title} menu={menu} />;
-};
 
-const makeMapStateToProps = () => {
+const Component: React.FC = (props) => {
   const selectTheme = makeSelectTheme();
-  return (state) => ({
-    title: selectTheme(state),
-  });
+  const title = useSelector(selectTheme);
+  const menu = makeDropdownMenu();
+  return <Wrapper title={title} menu={menu} />;
 };
-const mapDispatchToProps = (dispatch) => ({
-  changeTheme: (theme) => dispatch(changeTheme(theme)),
-});
 
-type Props = ReturnType<ReturnType<typeof makeMapStateToProps>> &
-  ReturnType<typeof mapDispatchToProps> & {
-    title: string;
-  };
-
-export default connect(makeMapStateToProps, mapDispatchToProps)(Component);
+export default Component;
--- src/app/containers/ThemeDropdown/makeDropdownMenu.tsx
+++ src/app/containers/ThemeDropdown/makeDropdownMenu.tsx
@@ -1,11 +1,16 @@
 import { Menu } from 'antd';
 import React from 'react';
 import { themeOptions } from 'app/store/style/reducer';
+import { useDispatch } from 'react-redux';
+import { changeTheme } from 'store/style/actions';
 
-export default (changeTheme) => (
-  <Menu onClick={({ key }) => changeTheme(key)}>
+export default () => {
+  const dispatch = useDispatch()
+  return (
+  <Menu onClick={({ key }) => dispatch(changeTheme(key))}>
     {themeOptions.map((t) => (
       <Menu.Item key={t}>{t}</Menu.Item>
     ))}
   </Menu>
-);
+)
+};
