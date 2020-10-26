594217227a06af67fdfbd96d37e0c46463459b71
Summer Chen <mp4bj6ruby@gmail.com>
2020-10-26 16:52:46 +0800

    components > RelativeDateBtns: style modify

--- src/components/RelativeDateBtns.tsx
+++ src/components/RelativeDateBtns.tsx
@@ -4,13 +4,13 @@ import { Button, Space } from 'antd';
 const PageSearchBar: React.FC = (props) => {
   return (
     <Space size="small" {...props}>
-      <Button type="primary">昨天</Button>
+      <Button>昨天</Button>
       <Button type="primary">今天</Button>
-      <Button type="primary">明天</Button>
-      <Button type="primary">上週</Button>
-      <Button type="primary">本週</Button>
-      <Button type="primary">上月</Button>
-      <Button type="primary">本月</Button>
+      <Button>明天</Button>
+      <Button>上週</Button>
+      <Button>本週</Button>
+      <Button>上月</Button>
+      <Button>本月</Button>
     </Space>
   );
 };
