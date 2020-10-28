095ef63617284d48ac917a84d719eb6ae81c8d1d
Summer Chen <mp4bj6ruby@gmail.com>
2020-10-28 10:05:20 +0800

    dynamic reducer in historyReport page

--- src/pages/report/History/index.tsx
+++ src/pages/report/History/index.tsx
@@ -7,8 +7,13 @@ import SearchForm from './SearchForm';
 import PageSearchBar from '@/components/PageSearchBar';
 import EmptyDivider from '@/components/EmptyDivider';
 import RelativeDateBtns from '@/components/RelativeDateBtns';
+import { StoreType } from '@/store';
+import { useStore } from 'react-redux';
+import historyReportReducer, { moduleName } from './reducer';
 
 const Component: React.FC = () => {
+  const store: StoreType = useStore();
+  store.injectReducer(moduleName, historyReportReducer);
   return (
     <Dashboard>
       <PageHeader />
